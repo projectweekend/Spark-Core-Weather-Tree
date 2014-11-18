7a548a80a2df63850fc785e0cbe561b0791c0b55
Brian Hines <brian@projectweekend.net>
2014-11-18 05:51:11 -0600

    Start making API calls to Spark Core

--- Python/check_weather.py
+++ Python/check_weather.py
@@ -1,34 +1,50 @@
 import os
+import sys
 
 import requests
 import forecastio
+from pyecho import echo, FailingTooHard
 
 
 FORECAST_IO_KEY = os.getenv('FORECAST_IO_KEY')
+assert FORECAST_IO_KEY
+
 FORECAST_IO_LATITUDE = os.getenv('FORECAST_IO_LATITUDE')
+assert FORECAST_IO_LATITUDE
+
 FORECAST_IO_LONGITUDE = os.getenv('FORECAST_IO_LONGITUDE')
+assert FORECAST_IO_LONGITUDE
+
 SPARK_DEVICE_ID = os.getenv('SPARK_DEVICE_ID')
+assert SPARK_DEVICE_ID
+
 SPARK_AUTH_TOKEN = os.getenv('SPARK_AUTH_TOKEN')
+assert SPARK_AUTH_TOKEN
+
+SPARK_URL_TEMPLATE = "https://api.spark.io/v1/devices/{0}/{1}"
 
 
+@echo(5)
 def send_to_spark(function_name):
-	print(function_name)
+	url = SPARK_URL_TEMPLATE.format(SPARK_DEVICE_ID, function_name)
+	headers = {'Authorization': "Bearer {0}".format(SPARK_AUTH_TOKEN)}
+	return requests.post(url, headers=headers)
 
 
 def report_snow():
-	send_to_spark('snow')
+	return send_to_spark('snow')
 
 
 def report_sleet():
-	send_to_spark('sleet')
+	return send_to_spark('sleet')
 
 
 def report_rain():
-	send_to_spark('rain')
+	return send_to_spark('rain')
 
 
 def report_no_percipitation():
-	send_to_spark('noPercipitation')
+	return send_to_spark('none')
 
 
 def main():
@@ -43,9 +59,19 @@ def main():
 	}
 
 	try:
-		return percipitation[currently.d['icon']]()
-	except KeyError:
-		return report_no_percipitation()
+		try:
+			response = percipitation[currently.d['icon']]()
+		except KeyError:
+			response = report_no_percipitation()
+	except FailingTooHard:
+		# TODO: send email, blink LED...something to alert me that request is failing
+		sys.exit(1)
+
+	if response.status_code != 200:
+		# TODO: send email, blink LED...something to alert me that request is bad
+		sys.exit(1)
+
+	sys.exit(0)
 
 
 if __name__ == '__main__':
