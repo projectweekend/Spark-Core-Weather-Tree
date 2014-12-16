eb0d342770043b7737260822e4261f1837bd418b
Brian Hines <brian@projectweekend.net>
2014-12-15 20:07:50 -0600

    Let's really use this thing

--- Arduino/weather-tree.ino
+++ Arduino/weather-tree.ino
@@ -2,17 +2,19 @@
 #include "PixelControl.h"
 
 
-Adafruit_NeoPixel neopixelRingLarge = Adafruit_NeoPixel(24, 4, WS2812);
+Adafruit_NeoPixel neopixelRingLarge = Adafruit_NeoPixel(24, 6, WS2812);
 PixelControl ringLarge = PixelControl();
 
 Adafruit_NeoPixel neopixelRingMedium = Adafruit_NeoPixel(16, 5, WS2812);
 PixelControl ringMedium = PixelControl();
 
-Adafruit_NeoPixel neopixelRingSmall = Adafruit_NeoPixel(12, 6, WS2812);
+Adafruit_NeoPixel neopixelRingSmall = Adafruit_NeoPixel(12, 4, WS2812);
 PixelControl ringSmall = PixelControl();
 
 // This is the pixel on the top...it will be controlled differently: TODO
 Adafruit_NeoPixel neopixelSingle = Adafruit_NeoPixel(1, 7, WS2812);
+
+int currentPercipitation = 0;
 
 
 void setup()
@@ -29,6 +31,13 @@ void setup()
     neopixelSingle.begin();
     neopixelSingle.show();
 
+    // Default color
+    // ringSmall.ColorWipe(neopixelRingSmall, neopixelRingLarge.Color(0, 255, 0), 100);
+    // ringMedium.ColorWipe(neopixelRingMedium, neopixelRingMedium.Color(0, 255, 0), 100);
+    // ringLarge.ColorWipe(neopixelRingLarge, neopixelRingSmall.Color(0, 255, 0), 100);
+
+    int result = noPercipitation();
+
     randomSeed(analogRead(0));
 
     Spark.function("rain", rain);
@@ -39,35 +48,53 @@ void setup()
 
 void loop()
 {
+    switch (currentPercipitation) {
+        case 1:
+            // Rain
+            ringSmall.AnimateRandom(neopixelRingSmall, neopixelRingLarge.Color(0, 0, 255), 100);
+            ringMedium.AnimateRandom(neopixelRingMedium, neopixelRingMedium.Color(0, 0, 255), 100);
+            ringLarge.AnimateRandom(neopixelRingLarge, neopixelRingSmall.Color(0, 0, 255), 100);
+            break;
+        case 2:
+            // Snow
+            ringSmall.AnimateRandom(neopixelRingSmall, neopixelRingLarge.Color(127, 127, 127), 100);
+            ringMedium.AnimateRandom(neopixelRingMedium, neopixelRingMedium.Color(127, 127, 127), 100);
+            ringLarge.AnimateRandom(neopixelRingLarge, neopixelRingSmall.Color(127, 127, 127), 100);
+            break;
+    }
+}
 
+
+int noPercipitation(String args)
+{
+    clear();
+    ringSmall.ColorWipe(neopixelRingSmall, neopixelRingLarge.Color(0, 255, 0), 100);
+    ringMedium.ColorWipe(neopixelRingMedium, neopixelRingMedium.Color(0, 255, 0), 100);
+    ringLarge.ColorWipe(neopixelRingLarge, neopixelRingSmall.Color(0, 255, 0), 100);
+    currentPercipitation = 0;
+    return 200;
 }
 
 
 int rain(String args)
 {
-    ringSmall.AnimateRandom(neopixelRingSmall, neopixelRingLarge.Color(0, 0, 255), 100);
-    ringMedium.AnimateRandom(neopixelRingMedium, neopixelRingMedium.Color(0, 0, 255), 100);
-    ringLarge.AnimateRandom(neopixelRingLarge, neopixelRingSmall.Color(0, 0, 255), 100);
-
+    clear();
+    currentPercipitation = 1;
     return 200;
 }
 
 
 int snow(String args)
 {
-    ringSmall.AnimateRandom(neopixelRingSmall, neopixelRingLarge.Color(127, 127, 127), 100);
-    ringMedium.AnimateRandom(neopixelRingMedium, neopixelRingMedium.Color(127, 127, 127), 100);
-    ringLarge.AnimateRandom(neopixelRingLarge, neopixelRingSmall.Color(127, 127, 127), 100);
-
+    clear();
+    currentPercipitation = 2;
     return 200;
 }
 
 
-int noPercipitation(String args)
+void clear()
 {
-    ringSmall.ColorWipe(neopixelRingSmall, neopixelRingLarge.Color(0, 255, 0), 100);
-    ringMedium.ColorWipe(neopixelRingMedium, neopixelRingMedium.Color(0, 255, 0), 100);
-    ringLarge.ColorWipe(neopixelRingLarge, neopixelRingSmall.Color(0, 255, 0), 100);
-
-    return 200;
+    ringSmall.ColorWipe(neopixelRingSmall, 0, 0);
+    ringMedium.ColorWipe(neopixelRingMedium, 0, 0);
+    ringLarge.ColorWipe(neopixelRingLarge, 0, 0);
 }
